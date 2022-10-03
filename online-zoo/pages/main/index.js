56d40d28f248064e0dc43171c5f52fca55cb1aad
Syderi <syderi@yahoo.com>
2022-10-03 15:14:31 +0300

    fix: slider

--- online-zoo/pages/main/index.js
+++ online-zoo/pages/main/index.js
@@ -1,3 +1,22 @@
-
+import animals from '../../assets/js/animals.js'
 
 console.log('Hello')
+console.log(animals)
+
+const imgCardPanda = document.querySelector('.img_card_panda')
+const sliderCards = document.querySelector('.slider__cards')
+
+
+imgCardPanda.src = animals[0].image;
+
+
+function createCard (params) {
+   let sliderCard = document.createElement('div');
+   sliderCard.classList.add('slider__card')
+   sliderCards.append(sliderCard)
+
+   let imgCard = document.createElement('img');
+   imgCard
+}
+
+createCard()
