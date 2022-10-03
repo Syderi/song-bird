a32002e056c544381f95bbdbddceff1d51aaf65a
Syderi <syderi@yahoo.com>
2022-10-03 11:47:37 +0300

    feat: popup

---
+++ online-zoo/assets/js/popup.js
@@ -0,0 +1,13 @@
+console.log('Hello from popup')
+
+const popupClicks = document.querySelectorAll('.popup-click');
+const popup = document.querySelector('.popup');
+console.log(popupClicks)
+
+
+
+popupClicks.forEach(popupClick => {
+    popupClick.addEventListener('click', () => {
+        popup.classList.toggle('popup-active')
+    })
+})
