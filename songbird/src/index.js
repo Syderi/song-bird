ec3dac51e846b94147de8b2420a0689e6e0eef89
Syderi <syderi@yahoo.com>
2022-10-30 17:41:06 +0300

    refactor: add style for header

--- songbird/src/index.js
+++ songbird/src/index.js
@@ -3,3 +3,11 @@ import "./assets/js/_addFavicon";
 
 import "../index.html";
 import "./style.scss";
+
+const langSelect = document.getElementById("lang")
+console.log(langSelect)
+
+
+langSelect.addEventListener("change", () => {
+console.log(langSelect.value)
+})
