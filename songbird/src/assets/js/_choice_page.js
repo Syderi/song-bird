79138ed88aed08ed9926efc57b7d598061bc71f3
Syderi <syderi@yahoo.com>
2022-11-03 08:32:35 +0300

    feat: add count Score Game

--- songbird/src/assets/js/_choice_page.js
+++ songbird/src/assets/js/_choice_page.js
@@ -1,56 +1,23 @@
 // import birdsData from "./data/_birdsData_en"
 
-// main__wrapper
-// header__nav
-
-// active
-
-
-// window.onload = function() {
-
-
-
-// }
-
-
-// main-section_hide
-
-// const mainWrapper = document.querySelector(".main__wrapper")
-// console.log(mainWrapper)
-
-
-// console.log("Array.from(mainWrapper.children)",Array.from(mainWrapper.children))
-// console.log("Array.from(mainWrapper.children).indexOf(numberli)",Array.from(mainWrapper.children).indexOf(numberli))
-
-
 const addTagsClickHandler = () => {
     const headerNav = document.querySelector('.header__nav');
     const mainWrapper = document.querySelector(".main__wrapper")
     const mainWrapperChildern = Array.from(mainWrapper.children);
     headerNav.addEventListener('click', (e) => {
         if (e.target.classList.contains('nav__item')) {
-
             let clickedTag = e.target;
             removeSelectedTags();
             selectClickedTag(clickedTag);
-
-
             const numberli = e.target.closest(".nav__item");
-            // console.log(numberli)
             const index = Array.from(headerNav.children).indexOf(numberli);
-            // console.log(index)
-
-
             removeSection(mainWrapperChildern)
             addSection(mainWrapperChildern[index])
-
-
-
         }
     })
 }
 
-    addTagsClickHandler();
+addTagsClickHandler();
 
 const removeSelectedTags = () => {
     let tags = document.querySelectorAll('.nav__item');
--- songbird/src/assets/js/_game.js
+++ songbird/src/assets/js/_game.js
@@ -4,38 +4,58 @@ import {createGameCard} from "./_create_gallery"
 import {createRandomCard} from "./_create_random_card"
 
 
+// Начало ОСНОВНЫЕ КОНСТАНТЫ
+const gameChoiseConteiner = document.querySelector(".game__choice")
+const gameNextButton = document.querySelector(".game__next-button")
+const gameScoreCount = document.querySelector(".game__score__count")
+let gameScore = 0;
+let countLoop = 0;
+let keyBlockChoiseColor = false
+let numberOfAttempts = 5
+
+let mainArray = birdsDataRU.slice() // Создал копию массива от базы. ссылки объекты
+let loopArray = getloopArray(countLoop) // Взял часть массива
+let randomLoopValue = getRandomNum(0, 6) // Правильный ответ 0-5
+
 // Создал карточку рандомную
 const mainGame = document.querySelector(".main__game")
-const gameCardRandom = createRandomCard(birdsDataRU, 37, false)
+const gameCardRandom = createRandomCard(mainArray, 37, false)
 mainGame.insertBefore(gameCardRandom, mainGame.children[1])
 // Конец создания карточеки рандомной
 
 // Создал карточку просмотра ответов
 const gamePlayWrapper = document.querySelector(".game__play__wrapper")
-const gameCardgame = createGameCard(birdsDataRU,37)
+let gameCardgame = createGameCard(mainArray,37)
 gamePlayWrapper.insertBefore(gameCardgame, gamePlayWrapper.children[1])
 // Конец создания карточек просмотра ответа
 
-// Начало Основные константы
-let gameScore = 0;
-let countLoop = 0;
-let mainArray = birdsDataRU.slice()
-console.log("mainArray",mainArray)
-// Конец Основные константы
+// Начало Функция заполнения вопросов
+function repaceGameChoiceItem(array = loopArray) {
+   array.forEach((element,index,arr) => {
+      gameChoiseConteiner.children[index].innerHTML = `<span class="game__choice__item-btn"></span>${element.name}`
+   });
+   const gameChoiceItemSpan = document.querySelectorAll(".game__choice__item-btn")
+   array.forEach((element,index,arr) => {
+      gameChoiceItemSpan[index].className = "game__choice__item-btn"
+
+   });
+};
+repaceGameChoiceItem()
+// Конец Функция заполнения вопросов
+
+// Конец ОСНОВНЫЕ КОНСТАНТЫ
 
 // Начало функция возврата части главного массива
-function getloopArray(currentLoop) {
+function getloopArray(currentLoop = countLoop) {
 return mainArray.slice(currentLoop*6,((currentLoop*6)+6))
 }
 // Конец функция возврата части главного массива
 
-// Взял часть массива
-let loopArray = getloopArray(countLoop)
-console.log("loopArray",loopArray)
-// Конец взятия части массива
-
-let randomLoopValue = getRandomNum(0, 6)
-console.log("randomLoopValue",randomLoopValue)
+// Начало функция возврата ID главного массива
+function getmainArrayID (index, currentLoop = countLoop) {
+return (currentLoop*6 + index + 1)
+}
+// Конец функция возврата ID главного массива
 
 // Начало Функция случайного числа
 function getRandomNum(min, max) {
@@ -43,42 +63,108 @@ function getRandomNum(min, max) {
 };
 // Конец Функция случайного числа
 
-// console.log(loopArray[randomLoopValue]);
-
+// Начало фунции заполнения игры Создаю рандом и карту с заданием 
 function createGameWthisRandom(array = loopArray, randomValue = randomLoopValue) {
-   console.log(array[randomValue]);
    let id = array[randomValue].id
-   console.log(id);
-
-
-   // const mainGame = document.querySelector(".main__game")
-   // const gameCardRandom = createRandomCard(birdsDataRU, 37, false)
-   // mainGame.insertBefore(gameCardRandom, mainGame.children[1])
-
-
+   gameCardgame = createGameCard(mainArray,37)
    mainGame.children[1].replaceWith(createRandomCard(mainArray, id, false))
-   gamePlayWrapper.children[1].replaceWith(createGameCard(mainArray,37))
+   gamePlayWrapper.children[1].replaceWith(gameCardgame)
 }
 
 createGameWthisRandom()
+// Конец фунции заполнения игры
+
+// Начало слушатель кликов на выборе птиц
+gameChoiseConteiner.addEventListener("click", (e) => {
+if (e.target.classList.contains('game__choice__item')) {
+   const gameChoiceItemSpan = document.querySelectorAll(".game__choice__item-btn")
+   let clickedTag = e.target;
+   const numberli = clickedTag.closest(".game__choice__item");
+   const indexNumberLi = Array.from(gameChoiseConteiner.children).indexOf(numberli);
+   const mainArrayID = getmainArrayID(indexNumberLi)
+
+   gamePlayWrapper.children[1].replaceWith(createGameCard(mainArray, mainArrayID))
+
+
+   // console.log(gameChoiceItemSpan[indexNumberLi].classList.contains("game__choice__item-btn_red"))
+
+// console.log(randomLoopValue)
+if (randomLoopValue === indexNumberLi) {
+   if (!keyBlockChoiseColor) {
+         gameChoiceItemSpan[indexNumberLi].classList.add("game__choice__item-btn_green")
+         gameScore =+ numberOfAttempts
+
+         gameScoreCount.textContent = gameScore
+         mainGame.children[1].replaceWith(createRandomCard(mainArray, mainArrayID, true))
+         gameNextButton.disabled = false
+   }
+   keyBlockChoiseColor = !keyBlockChoiseColor
+
+} else {
+   if (!keyBlockChoiseColor) {
+      if (!gameChoiceItemSpan[indexNumberLi].classList.contains("game__choice__item-btn_red")) {
+                     numberOfAttempts --
+      }
+            gameChoiceItemSpan[indexNumberLi].classList.add("game__choice__item-btn_red")
+
+   }
+
+}
+}
+console.log("numberOfAttempts====",numberOfAttempts)
+})
+// Конец слушатель кликов на выборе птиц
+
+
+// Начало слушатель на Главном Баттоне
+gameNextButton.addEventListener("click", ()=> {
+console.log("УРА КНОПКА ЗАРАБОТАЛА")
+countLoop++
+console.log("countLoop",countLoop)
+})
+// Конец  слушатель на Главном Баттоне
+
+
+
+// gameCardgame.addEventListener("click", (e) => {
+//    console.log("10000===",e);
+// })
+
+
 
 // setTimeout(() => {
-//     gamePlayWrapper.children[1].remove()
-//     const gameCardgame = createGameCard(birdsDataRU,37)
-//     // gameCardgame.audio.play = false
-//     console.log(gameCardgame)
-//     gamePlayWrapper.insertBefore(gameCardgame, gamePlayWrapper.children[1])
-// }, 5000);
-// setTimeout(() => {
+//    gameCardgame = createGameCard(birdsDataRU,16)
+//    gameCardgame.addEventListener("click", (e) => {
+//       console.log("10000===",e);
+//    })
+   
 //     gamePlayWrapper.children[1].replaceWith(createGameCard(birdsDataRU,16))
 // }, 10000);
 // setTimeout(() => {
+//    gameCardgame = createGameCard(birdsDataRU,17)
+//    gameCardgame.addEventListener("click", (e) => {
+//       console.log("10000===",e);
+//    })
+   
 //     gamePlayWrapper.children[1].replaceWith(createGameCard(birdsDataRU,17))
-// }, 15000);
+// }, 10000);
 // setTimeout(() => {
+//    gameCardgame = createGameCard(birdsDataRU,18)
+//    gameCardgame.addEventListener("click", (e) => {
+//       console.log("10000===",e);
+//    })
+   
 //     gamePlayWrapper.children[1].replaceWith(createGameCard(birdsDataRU,18))
-// }, 20000);
-// gameCardgame.replaceWith(createGameCard(birdsDataRU,35))
+// }, 10000);
+// setTimeout(() => {
+//    gameCardgame.addEventListener("click", (e) => {
+//       console.log("10000===",e);
+//    })
+   
+//    gameCardgame = createGameCard(birdsDataRU,19)
+//     gamePlayWrapper.children[1].replaceWith(createGameCard(birdsDataRU,19))
+// }, 10000);
+
 
 // Начало основной фунции игры
 
@@ -99,13 +185,28 @@ createGameWthisRandom()
 
 
 
-// Начало Функция перемешивания массива
-function shuffle(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-       const j = Math.floor(Math.random() * (i + 1));
-       [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
- }
- // Конец Функция перемешивания массива
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Начало Функция перемешивания массива
+// function shuffle(array) {
+//     for (let i = array.length - 1; i > 0; i--) {
+//        const j = Math.floor(Math.random() * (i + 1));
+//        [array[i], array[j]] = [array[j], array[i]];
+//     }
+//     return array;
+//  }
+//  // Конец Функция перемешивания массива
 
