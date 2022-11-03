b8d2e8c48f66d3bb30f3f4f17d64eb804d5f7e32
Syderi <syderi@yahoo.com>
2022-11-03 13:21:08 +0300

    feat: add logik game and audio

--- songbird/src/assets/js/_game.js
+++ songbird/src/assets/js/_game.js
@@ -2,9 +2,19 @@ import birdsDataEN from "./data/_birdsData_en"
 import birdsDataRU from "./data/_birdsData_ru"
 import {createGameCard} from "./_create_gallery"
 import {createRandomCard} from "./_create_random_card"
+import correctAnswer from "../audio/pravilnyiy-otvet.mp3"
+import wrongAnswer from "../audio/nepravilnyiy-otvet.mp3"
 
 
 // Начало ОСНОВНЫЕ КОНСТАНТЫ
+const correctAudio = new Audio(correctAnswer)
+// correctAudio.play()
+correctAudio.volume = 0.5
+
+const wrongAudio = new Audio(wrongAnswer)
+// wrongAudio.play()
+wrongAudio.volume = 0.9
+
 const gameChoiseConteiner = document.querySelector(".game__choice")
 const gameNextButton = document.querySelector(".game__next-button")
 const gameScoreCount = document.querySelector(".game__score__count")
@@ -16,6 +26,7 @@ let numberOfAttempts = 5
 let mainArray = birdsDataRU.slice() // Создал копию массива от базы. ссылки объекты
 let loopArray = getloopArray(countLoop) // Взял часть массива
 let randomLoopValue = getRandomNum(0, 6) // Правильный ответ 0-5
+console.log("ПЕРвЫЙ РАНДОМ ОТВЕТ randomLoopValue", randomLoopValue)
 
 // Создал карточку рандомную
 const mainGame = document.querySelector(".main__game")
@@ -29,7 +40,7 @@ let gameCardgame = createGameCard(mainArray,37)
 gamePlayWrapper.insertBefore(gameCardgame, gamePlayWrapper.children[1])
 // Конец создания карточек просмотра ответа
 
-// Начало Функция заполнения вопросов
+// Начало Функция заполнения  вопросов и обновления
 function repaceGameChoiceItem(array = loopArray) {
    array.forEach((element,index,arr) => {
       gameChoiseConteiner.children[index].innerHTML = `<span class="game__choice__item-btn"></span>${element.name}`
@@ -74,9 +85,37 @@ function createGameWthisRandom(array = loopArray, randomValue = randomLoopValue)
 createGameWthisRandom()
 // Конец фунции заполнения игры
 
+// Функция перемотки аудио ошибок
+function resetAudioDurationWrongCorrect() {
+   wrongAudio.pause();
+   wrongAudio.currentTime = 0;
+   correctAudio.pause();
+   correctAudio.currentTime = 0;
+}
+// Конец фунции перемотки аудио ошибок
+
+// Начало фунций выделения вопроса
+function changeSelectedTagsQuestion(countLoop) {
+   let tags = document.querySelectorAll('.game__select__item');
+   tags.forEach(tag => {
+       tag.classList.remove('active');
+   })
+   tags[countLoop].classList.add('active')
+}
+
+// const selectClickedTagQuestion = (countLoop) => {
+//    clickedTag.classList.add('active');
+// }
+
+// Конец функций выделения вопроса
+
+
+
 // Начало слушатель кликов на выборе птиц
 gameChoiseConteiner.addEventListener("click", (e) => {
+
 if (e.target.classList.contains('game__choice__item')) {
+   resetAudioDurationWrongCorrect()
    const gameChoiceItemSpan = document.querySelectorAll(".game__choice__item-btn")
    let clickedTag = e.target;
    const numberli = clickedTag.closest(".game__choice__item");
@@ -85,25 +124,28 @@ if (e.target.classList.contains('game__choice__item')) {
 
    gamePlayWrapper.children[1].replaceWith(createGameCard(mainArray, mainArrayID))
 
-
-   // console.log(gameChoiceItemSpan[indexNumberLi].classList.contains("game__choice__item-btn_red"))
-
-// console.log(randomLoopValue)
 if (randomLoopValue === indexNumberLi) {
    if (!keyBlockChoiseColor) {
-         gameChoiceItemSpan[indexNumberLi].classList.add("game__choice__item-btn_green")
-         gameScore =+ numberOfAttempts
 
+
+
+      if (!gameChoiceItemSpan[indexNumberLi].classList.contains("game__choice__item-btn_green")) {
+         correctAudio.play();
+         gameScore = gameScore + numberOfAttempts
          gameScoreCount.textContent = gameScore
          mainGame.children[1].replaceWith(createRandomCard(mainArray, mainArrayID, true))
          gameNextButton.disabled = false
+
+      }
+         gameChoiceItemSpan[indexNumberLi].classList.add("game__choice__item-btn_green")
    }
    keyBlockChoiseColor = !keyBlockChoiseColor
 
 } else {
    if (!keyBlockChoiseColor) {
       if (!gameChoiceItemSpan[indexNumberLi].classList.contains("game__choice__item-btn_red")) {
-                     numberOfAttempts --
+         wrongAudio.play()            
+         numberOfAttempts --
       }
             gameChoiceItemSpan[indexNumberLi].classList.add("game__choice__item-btn_red")
 
@@ -112,15 +154,36 @@ if (randomLoopValue === indexNumberLi) {
 }
 }
 console.log("numberOfAttempts====",numberOfAttempts)
+console.log("gameScore====",gameScore)
 })
 // Конец слушатель кликов на выборе птиц
 
 
 // Начало слушатель на Главном Баттоне
 gameNextButton.addEventListener("click", ()=> {
-console.log("УРА КНОПКА ЗАРАБОТАЛА")
 countLoop++
-console.log("countLoop",countLoop)
+// console.log("countLoop",countLoop)
+keyBlockChoiseColor = false
+// console.log("keyBlockChoiseColor",keyBlockChoiseColor)
+numberOfAttempts = 5
+// console.log("numberOfAttempts BTN",numberOfAttempts)
+loopArray = getloopArray(countLoop) // Взял часть массива
+// console.log("loopArray BTN",loopArray)
+randomLoopValue = getRandomNum(0, 6)
+// console.log("randomLoopValue BTN",randomLoopValue)
+// console.log("countLoop BTN",countLoop)
+
+repaceGameChoiceItem()
+// let mainArrayID = getmainArrayID(randomLoopValue)
+// console.log("mainArrayID",mainArrayID)
+
+createGameWthisRandom()
+changeSelectedTagsQuestion(countLoop)
+
+// gamePlayWrapper.children[1].replaceWith(createGameCard(mainArray, 37))
+// mainGame.children[1].replaceWith(createRandomCard(mainArray, mainArrayID, false))
+
+gameNextButton.disabled = true
 })
 // Конец  слушатель на Главном Баттоне
 
