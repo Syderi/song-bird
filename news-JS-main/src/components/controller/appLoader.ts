8904c377fe6d766a7e4642b5014bf6dec6590d66
Syderi <syderi@yahoo.com>
2022-12-02 21:35:43 +0300

    fix: link app and add links in footer

--- news-JS-main/src/components/controller/appLoader.ts
+++ news-JS-main/src/components/controller/appLoader.ts
@@ -2,7 +2,8 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://nodenews.herokuapp.com/', {
+        super('https://newsapi-redirect-production.up.railway.app/', {
+        // super('https://nodenews.herokuapp.com/', {
         // super('https://newsapi.org/v2/', {
             apiKey: 'beb9565b064d45e59d52c0a9c93178cf', // получите свой ключ https://newsapi.org/
         });
